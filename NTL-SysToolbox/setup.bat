34c795aadb202d11dac8a0c4719de98e15366d27
Laura Poulain <laurapoulain05@gmail.com>
2026-01-06 18:07:26 +0100

    fichier bat final

--- NTL-SysToolbox/setup.bat
+++ NTL-SysToolbox/setup.bat
@@ -10,15 +10,19 @@ if %errorlevel% neq 0 (
 )
 
 :: Crée un environnement virtuel
+echo === Creation de l'environnement ===
 python -m venv venv_ntl
 
 :: Active le venv
+echo === Activation ===
 call venv_ntl\Scripts\activate
 
 :: Installe les dépendances
+echo === Installation des dependances ===
 pip install -r .requirements.txt
 
 :: Lance le programme
+echo === Lancement du programme ===
 python Dev\main.py
 
 echo === Fin de l'installation ===

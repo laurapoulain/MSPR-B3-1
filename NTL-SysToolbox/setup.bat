25f312b5f866b5a4b67debdd719a4082e15c4ed7
Laura Poulain <laurapoulain05@gmail.com>
2026-01-06 17:58:08 +0100

    fichier de lancement .bat windows ajout etapes visuelles

---
+++ NTL-SysToolbox/setup.bat
@@ -0,0 +1,25 @@
+@echo off
+echo === Installation de NTL-SysToolbox (Windows) ===
+
+:: Vérifie Python
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo [ERREUR] Python non détecté. Installez Python 3 avant de continuer. https://www.python.org/downloads/windows/
+    pause
+    exit /b
+)
+
+:: Crée un environnement virtuel
+python -m venv venv_ntl
+
+:: Active le venv
+call venv_ntl\Scripts\activate
+
+:: Installe les dépendances
+pip install -r .requirements.txt
+
+:: Lance le programme
+python Dev\main.py
+
+echo === Fin de l'installation ===
+pause

50ba29b469ee42516c5756d13d5ed0b8597fcd9f
Laura Poulain <laurapoulain05@gmail.com>
2026-01-09 14:12:50 +0100

    ajout d'une fonction ask_existing_file pour gérer les erreurs de chemins saisis

--- NTL-SysToolbox/Dev/audit.py
+++ NTL-SysToolbox/Dev/audit.py
@@ -197,17 +197,38 @@ def list_os_versions(os_name: str, eol_db: Dict[str, Dict[str, str]]) -> None:
         console.print(f"- [white]{os_name} {version}[/white] : EOL = [bold]{eol}[/bold]")
 
 
+# Fonction permettant de gérer les erreurs de noms de fichiers rentrés par l'utilisateur 
+def ask_existing_file(prompt: str, default: str = "") -> str:
+    while True:
+        path = console.input(prompt).strip() or default
+        if Path(path).is_file():
+            return path
+        console.print(
+            f"[bold red]✖ Fichier introuvable :[/bold red] [white]{path}[/white]"
+        )
+        console.input("[dim]Appuyez sur Entrée pour réessayer...[/dim]\n")
+
+
+
 # ===========================================================
 # Flows "métier" de l'audit
 # ===========================================================
 
 def run_csv_audit_flow() -> None:
     """Audit à partir d'un inventaire CSV."""
-    inventory_path = console.input("Chemin du fichier CSV d'inventaire (par défaut : outputs/fichier-existant.csv) : ").strip()
+    #inventory_path = console.input("Chemin du fichier CSV d'inventaire (par défaut : outputs/fichier-existant.csv) : ").strip()
+    inventory_path = ask_existing_file(
+    "Chemin du fichier CSV d'inventaire (par défaut : outputs/fichier-existant.csv) : ",
+    "outputs/fichier-existant.csv")
+
     console.print("\n[bold cyan][INFO][/bold cyan] Lecture de l'inventaire...")
     components = parse_inventory_csv(inventory_path)
 
-    eol_path = console.input("Chemin du fichier JSON EOL (par défaut : Data/eol.json) : ").strip()
+    #eol_path = console.input("Chemin du fichier JSON EOL (par défaut : Data/eol.json) : ").strip()
+    eol_path = ask_existing_file(
+    "Chemin du fichier JSON EOL (par défaut : Data/eol.json) : ",
+    "Data/eol.json")
+    
     output_csv = console.input("Chemin du rapport CSV de sortie (par défaut : outputs/votre_ficher.csv): ").strip()
     output_json = console.input("Chemin du rapport JSON de sortie (par défaut : outputs/votre_ficher.json) : ").strip()
     warning_months_str = console.input(
@@ -236,9 +257,21 @@ def run_scan_audit_flow() -> None:
     components = scan_network_simple(cidr)
     console.print(f"[bold cyan][INFO][/bold cyan] {len(components)} machine(s) détectée(s).")
 
-    eol_path = console.input("Chemin du fichier JSON EOL (par défaut : Data/eol.json) : ").strip()
-    output_csv = console.input("Chemin du rapport CSV de sortie (par défaut : outputs/votre_ficher.csv) : ").strip()
-    output_json = console.input("Chemin du rapport JSON de sortie (par défaut : outputs/votre_ficher.json) : ").strip()
+    #eol_path = console.input("Chemin du fichier JSON EOL (par défaut : Data/eol.json) : ").strip()
+    eol_path = ask_existing_file(
+    "Chemin du fichier JSON EOL (par défaut : Data/eol.json) : ",
+    "Data/eol.json")
+
+    #output_csv = console.input("Chemin du rapport CSV de sortie (par défaut : outputs/votre_ficher.csv) : ").strip()
+    output_csv = ask_existing_file(
+    "Chemin du rapport CSV de sortie (par défaut : outputs/votre_ficher.csv) : ",
+    "outputs/votre_ficher.csv")
+    
+    #output_json = console.input("Chemin du rapport JSON de sortie (par défaut : outputs/votre_ficher.json) : ").strip()
+    output_json = ask_existing_file(
+    "Chemin du rapport JSON de sortie (par défaut : outputs/votre_ficher.json) : ",
+    "outputs/votre_ficher.json")
+    
     warning_months_str = console.input(
         "Nombre de mois avant EOL pour passer en 'warning' (12 par défaut) : "
     ).strip()
@@ -261,7 +294,11 @@ def run_scan_audit_flow() -> None:
 
 def run_eol_consult_flow() -> None:
     """Consultation des EOL pour un OS."""
-    eol_path = console.input("Chemin du fichier JSON EOL (par défaut : Data/eol.json) : ").strip()
+    #eol_path = console.input("Chemin du fichier JSON EOL (par défaut : Data/eol.json) : ").strip()
+    eol_path = ask_existing_file(
+    "Chemin du fichier JSON EOL (par défaut : Data/eol.json) : ",
+    "Data/eol.json")
+    
     eol_db = load_eol_database(eol_path)
     os_name = console.input("Nom de l'OS (ex: 'Windows Server' ou 'Ubuntu') : ").strip()
     console.print("")  # petite ligne vide
